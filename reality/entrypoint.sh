0e97bf358fcaa90fd4ac16465ea51bbf11bddc6d
DamnCrab <42539593+damncrab@users.noreply.github.com>
2025-12-15 15:46:18 +0800

    ♻️refactor(docker): 改进日志管理与配置文件路径

--- reality/entrypoint.sh
+++ reality/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-if [ -f /config_info.txt ]; then
+# Initialize config file if not exists (for volume mount)
+if [ ! -f /data/config.json ]; then
+  echo "Initializing config.json from template..."
+  cp /config.json.template /data/config.json
+fi
+
+if [ -f /data/config_info.txt ]; then
   echo "config.json exist"
 else
   IPV6=$(curl -6 -sSL --connect-timeout 3 --retry 2  ip.sb || echo "null")
@@ -17,7 +23,7 @@ else
 
   if [ -n "$HOSTMODE_PORT" ];then
     EXTERNAL_PORT=$HOSTMODE_PORT
-    jq ".inbounds[0].port=$HOSTMODE_PORT" /config.json >/config.json_tmp && mv /config.json_tmp /config.json
+    jq ".inbounds[0].port=$HOSTMODE_PORT" /data/config.json >/data/config.json_tmp && mv /data/config.json_tmp /data/config.json
   fi
 
   if [ -z "$DEST" ]; then
@@ -45,48 +51,48 @@ else
   fi
 
   # change config
-  jq ".inbounds[1].settings.clients[0].id=\"$UUID\"" /config.json >/config.json_tmp && mv /config.json_tmp /config.json
-  jq ".inbounds[1].streamSettings.realitySettings.dest=\"$DEST\"" /config.json >/config.json_tmp && mv /config.json_tmp /config.json
+  jq ".inbounds[1].settings.clients[0].id=\"$UUID\"" /data/config.json >/data/config.json_tmp && mv /data/config.json_tmp /data/config.json
+  jq ".inbounds[1].streamSettings.realitySettings.dest=\"$DEST\"" /data/config.json >/data/config.json_tmp && mv /data/config.json_tmp /data/config.json
 
   SERVERNAMES_JSON_ARRAY="$(echo "[$(echo $SERVERNAMES | awk '{for(i=1;i<=NF;i++) printf "\"%s\",", $i}' | sed 's/,$//')]")"
-  jq --argjson serverNames "$SERVERNAMES_JSON_ARRAY" '.inbounds[1].streamSettings.realitySettings.serverNames = $serverNames' /config.json >/config.json_tmp && mv /config.json_tmp /config.json
-  jq --argjson serverNames "$SERVERNAMES_JSON_ARRAY" '.routing.rules[0].domain = $serverNames' /config.json >/config.json_tmp && mv /config.json_tmp /config.json
+  jq --argjson serverNames "$SERVERNAMES_JSON_ARRAY" '.inbounds[1].streamSettings.realitySettings.serverNames = $serverNames' /data/config.json >/data/config.json_tmp && mv /data/config.json_tmp /data/config.json
+  jq --argjson serverNames "$SERVERNAMES_JSON_ARRAY" '.routing.rules[0].domain = $serverNames' /data/config.json >/data/config.json_tmp && mv /data/config.json_tmp /data/config.json
 
-  jq ".inbounds[1].streamSettings.realitySettings.privateKey=\"$PRIVATEKEY\"" /config.json >/config.json_tmp && mv /config.json_tmp /config.json
-  jq ".inbounds[1].streamSettings.network=\"$NETWORK\"" /config.json >/config.json_tmp && mv /config.json_tmp /config.json
+  jq ".inbounds[1].streamSettings.realitySettings.privateKey=\"$PRIVATEKEY\"" /data/config.json >/data/config.json_tmp && mv /data/config.json_tmp /data/config.json
+  jq ".inbounds[1].streamSettings.network=\"$NETWORK\"" /data/config.json >/data/config.json_tmp && mv /data/config.json_tmp /data/config.json
 
 
 
   FIRST_SERVERNAME=$(echo $SERVERNAMES | awk '{print $1}')
   # config info with green color
-  echo -e "\033[32m" >/config_info.txt
-  echo "IPV6: $IPV6" >>/config_info.txt
-  echo "IPV4: $IPV4" >>/config_info.txt
-  echo "UUID: $UUID" >>/config_info.txt
-  echo "DEST: $DEST" >>/config_info.txt
-  echo "PORT: $EXTERNAL_PORT" >>/config_info.txt
-  echo "SERVERNAMES: $SERVERNAMES (任选其一)" >>/config_info.txt
-  echo "PRIVATEKEY: $PRIVATEKEY" >>/config_info.txt
-  echo "PUBLICKEY/PASSWORD: $PUBLICKEY" >>/config_info.txt
-  echo "NETWORK: $NETWORK" >>/config_info.txt
+  echo -e "\033[32m" >/data/config_info.txt
+  echo "IPV6: $IPV6" >>/data/config_info.txt
+  echo "IPV4: $IPV4" >>/data/config_info.txt
+  echo "UUID: $UUID" >>/data/config_info.txt
+  echo "DEST: $DEST" >>/data/config_info.txt
+  echo "PORT: $EXTERNAL_PORT" >>/data/config_info.txt
+  echo "SERVERNAMES: $SERVERNAMES (任选其一)" >>/data/config_info.txt
+  echo "PRIVATEKEY: $PRIVATEKEY" >>/data/config_info.txt
+  echo "PUBLICKEY/PASSWORD: $PUBLICKEY" >>/data/config_info.txt
+  echo "NETWORK: $NETWORK" >>/data/config_info.txt
   if [ "$IPV4" != "null" ]; then
     SUB_IPV4="vless://$UUID@$IPV4:$EXTERNAL_PORT?encryption=none&security=reality&type=$NETWORK&sni=$FIRST_SERVERNAME&fp=chrome&pbk=$PUBLICKEY&flow=xtls-rprx-vision#${IPV4}-DamnCrab_docker_vless_reality_vision"
-    echo "IPV4 订阅连接: $SUB_IPV4" >>/config_info.txt
-    echo -e "IPV4 订阅二维码:\n$(echo "$SUB_IPV4" | qrencode -o - -t UTF8)" >>/config_info.txt
+    echo "IPV4 订阅连接: $SUB_IPV4" >>/data/config_info.txt
+    echo -e "IPV4 订阅二维码:\n$(echo "$SUB_IPV4" | qrencode -o - -t UTF8)" >>/data/config_info.txt
   fi
   if [ "$IPV6" != "null" ];then
     SUB_IPV6="vless://$UUID@$IPV6:$EXTERNAL_PORT?encryption=none&security=reality&type=$NETWORK&sni=$FIRST_SERVERNAME&fp=chrome&pbk=$PUBLICKEY&flow=xtls-rprx-vision#${IPV6}-DamnCrab_docker_vless_reality_vision"
-    echo "IPV6 订阅连接: $SUB_IPV6" >>/config_info.txt
-    echo -e "IPV6 订阅二维码:\n$(echo "$SUB_IPV6" | qrencode -o - -t UTF8)" >>/config_info.txt
+    echo "IPV6 订阅连接: $SUB_IPV6" >>/data/config_info.txt
+    echo -e "IPV6 订阅二维码:\n$(echo "$SUB_IPV6" | qrencode -o - -t UTF8)" >>/data/config_info.txt
   fi
 
 
-  echo -e "\033[0m" >>/config_info.txt
+  echo -e "\033[0m" >>/data/config_info.txt
 
 fi
 
 # show config info
-cat /config_info.txt
+cat /data/config_info.txt
 
 # run xray
-exec /xray -config /config.json
+exec /xray -config /data/config.json
